4dfbed55805d9cfd7cc3405ab9cd09c4e6c9d39b
Anders Wallenquist <anders.wallenquist@vertel.se>
2026-09-07 22:33:43 +0200

    [REF] Set 'Vertel Sverige AB' as author in module manifests
    Update author field to 'Vertel Sverige AB' (was 'Vertel AB' or missing).

--- website_slides_expiration/__manifest__.py
+++ website_slides_expiration/__manifest__.py
@@ -31,7 +31,7 @@
     'description': """
         This module makes it impossible to conitinue a slide when the course is expired. 
     """,
-    'author': 'Vertel AB',
+    'author': 'Vertel Sverige AB',
     'website': 'https://vertel.se/apps/odoo-e-learning',
     'images': ['static/description/banner.png'], # 560x280
     'license': 'AGPL-3',
--- website_slides_export_import/__manifest__.py
+++ website_slides_export_import/__manifest__.py
@@ -1,5 +1,6 @@
 {
     'name': 'eLearning Export Import',
+'author': 'Vertel Sverige AB',
     'version': '18.0.1.0.0',
     'category': 'eLearning',
     'summary': 'Export and import eLearning courses easily',
